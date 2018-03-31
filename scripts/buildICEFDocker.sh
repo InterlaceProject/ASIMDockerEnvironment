217a148b8fa39f7c84ce87db8b4863563caa51f4
hirsche <hirsch.eduard@gmail.com>
2018-03-31 13:07:46 +0200

    added clean target to maven call

--- scripts/buildICEFDocker.sh
+++ scripts/buildICEFDocker.sh
@@ -2,11 +2,11 @@
 
 #build core asim
 cd /home/icef/coreASIM/org.coreasim.parent && \
-      mvn package install
+      mvn clean package install
 
 #build eclipse plugin
 cd /home/icef/coreASIM/org.coreasim.eclipse && \
-      mvn package install
+      mvn clean package install
 
 #get/install missing node packages
 cd /home/icef/manager && \
